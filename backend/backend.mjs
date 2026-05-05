b1b67578dd7c9357e2d152d488c09812871bcaad
mettetal-charlotte <charlotte.mettetal@edu.univ-fcomte.fr>
2026-05-05 15:54:30 +0200

    image pocketbase

---
+++ backend/backend.mjs
@@ -0,0 +1,6 @@
+import PocketBase from 'pocketbase';
+export const pb = new PocketBase('https://tacita-pb.charlottemettetal.fr:11111');
+
+export async function getImageUrl(record, recordImage) {
+    return pb.files.getURL(record, recordImage);
+}
